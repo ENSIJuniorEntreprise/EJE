999bcf6dd44f89a94a6d8dc7259fb771b44a39db
ENSI Junior Entreprise <148913750+ENSIJuniorEntreprise@users.noreply.github.com>
2024-06-23 00:32:56 +0100

    Add files via upload

---
+++ src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+const BASE_URL = 'http://102.211.210.43/api';
+
+export default axios.create({
+    baseURL: BASE_URL
+});
+
+export const axiosPrivate = axios.create({
+    baseURL: BASE_URL,
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true
+});
